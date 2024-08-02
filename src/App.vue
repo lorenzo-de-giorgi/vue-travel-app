3acd915c5e81fdbcc09d073b1c13a5553aa4aa04
Degio <80683176+lorenzo-de-giorgi@users.noreply.github.com>
2024-08-02 13:22:07 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>ciao</h1>
+    <i class="fa fa-solid fa-home"></i>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
