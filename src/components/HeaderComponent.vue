8dbf23b7012e59442cfd096aa6ba2401c8ba5fce
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-08-02 13:39:18 +0200

    created HeaderComponent with navbar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,44 @@
 <template>
-    <div>
-
+    <div class="mt-nav">
+        <div class="container">
+            <nav class="d-flex justify-content-between align-items-center">
+                <img src="/public/logo.png" alt="logo" id="mt-logo">
+                <ul class="list-unstyled d-flex">
+                    <li class="mt-3 me-2"><a class="mt-delete" href="">Accedi</a></li>
+                    <li class="mt-3"><a class="mt-delete" href="">Registrati</a></li>
+                </ul>
+            </nav>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: 'HeaderComponent',
     }
 </script>
 
 <style lang="scss" scoped>
+    @use '../assets/styles/partials/variables' as *;
+
+    .mt-nav{
+        background-color: $teal-color;
+        #mt-logo{
+            width: 200px;
+        }
+        .mt-delete{
+            color: $white-color;
+            text-decoration: none;
+            border: 1px solid $white-color;
+            padding: 6px 12px;
+            border-radius: 12px;
+
+            &:hover{
+                background-color: $white-color;
+                color: $teal-color;
+                transition: 0.3s;
+            }
+        }
+    }
 
 </style>
